e04237528de0ce81f1b104d682e7dd165544a666
hionjk <artgeniy9@gmail.com>
2024-03-22 16:54:56 +0000

    Added different styles for Snake and Game Field. Optimized code

--- snake.pas
+++ snake.pas
@@ -9,12 +9,12 @@ const
   
   stdInputColor = LightRed;
   stdPrintColor = LightGreen;
-	
+
 	delayDuration = 75;
 	
 	maxFoodSatiation = 3;
+	maxStyleId = 3;
 	
-	borderChar = '#';
 type
 
 	TVector = record
@@ -29,8 +29,27 @@ type
 
 	TSnakePart = TVector;
 	
+	TField = record
+	  size: TSize;
+	 
+    style: record
+      id: integer;
+      borderColor: integer; 
+      borderChar: char;
+    end;
+	end;
+	
 	TSnake = record
-	  color: integer;
+	  style: record
+	    id: integer;
+	    
+	    headColor: integer;
+      bodyColor: integer;
+      
+      headChar: char;
+      bodyChar: char;   
+	  end;
+    
 		direction: TVector;
 		length: integer;
 		parts: array of TSnakePart;
@@ -57,19 +76,6 @@ begin
   end;
 end;
 
-procedure SetSettings(var fieldSize: TSize; var snake: TSnake);
-begin
-  TextColor(stdPrintColor);
-  write('Field width[',minFieldWidth,' - ', ScreenWidth - 5,']: ');
-  ReadInteger(minFieldWidth, ScreenWidth - 5, fieldSize.width, 
-  'You entered an incorrect field width');
-  
-  TextColor(stdPrintColor);
-  write('Field height[',minFieldHeight,' - ', ScreenHeight - 2,']: ');
-  ReadInteger(minFieldHeight, ScreenHeight - 2, fieldSize.height, 
-  'You entered an incorrect field height');
-end;
-
 procedure UpdateSnake(var snake: TSnake);
 var
   i: integer;
@@ -80,12 +86,10 @@ begin
     TextBackground(black);
     write(' ');  
   end;
-
+  
   for i := snake.length downto 2 do
   begin
     GotoXY(snake.parts[i - 1].x, snake.parts[i - 1].y);
-    write(' ');
-  
     snake.parts[i] := snake.parts[i - 1];
   end;
   
@@ -94,60 +98,101 @@ begin
   snake.parts[1].x := snake.parts[1].x + snake.direction.x;
   snake.parts[1].y := snake.parts[1].y + snake.direction.y;
   
-  TextBackground(snake.color);
+  
+  if snake.style.bodyChar = ' ' then
+    TextBackground(snake.style.bodyColor)
+  else
+    TextColor(snake.style.bodyColor);
+  
 	for i := 2 to snake.length do
 	begin
 		GotoXY(snake.parts[i].x, snake.parts[i].y);
-		write(' ') 
+		write(snake.style.bodyChar) 
 	end;
 	
-	TextBackground(Red);
+	if snake.style.headChar = ' ' then
+    TextBackground(snake.style.headColor)
+  else
+    TextColor(snake.style.headColor);
 	
 	GotoXY(snake.parts[1].x, snake.parts[1].y);
-	write(' ');
+	write(snake.style.headChar);
 	
 	GotoXY(1,1);
 end;
 
-procedure DrawBorder(size: TSize);
+procedure DrawField(field: TField);
 var
 	row, col: integer;
 begin
 	GotoXY(1, 1);
-  TextColor(LightGreen);
+  TextColor(field.style.borderColor);
   TextBackground(Black);
-	for col := 1 to size.width do
-		write(borderChar);
+	for col := 1 to field.size.width do
+		write(field.style.borderChar);
 		
 	writeln;
 	
-	for row := 1 to size.height - 1 do
+	for row := 1 to field.size.height - 1 do
 	begin
-		write(borderChar);
+		write(field.style.borderChar);
     
-    GotoXY(size.width, row);  
+    GotoXY(field.size.width, row);  
 			
-		writeln(borderChar);
+		writeln(field.style.borderChar);
 	end;
 	
-	for col := 1 to size.width do
-		write(borderChar);
+	for col := 1 to field.size.width do
+		write(field.style.borderChar);
 	
 	GotoXY(1,1);	
 end;	
 
+procedure SetFieldStyleSettings(var field: TField; 
+  borderChar: char; borderColor: integer);
+
+begin
+  field.style.borderChar := borderChar;
+  field.style.borderColor := borderColor;
+end;
+
+procedure InitField(var field: TField);
+begin
+  DrawField(field);
+  case field.style.id of
+    1: SetFieldStyleSettings(field, '#', Green);
+    2: SetFieldStyleSettings(field, '/', Green);
+    3: SetFieldStyleSettings(field, '|', Green);
+  end;
+end;
+
+procedure SetSnakeStyleSettings(var snake: TSnake; 
+headColor, bodyColor: integer;
+headChar, bodyChar: char);
+begin
+  snake.style.headColor := headColor;
+  snake.style.bodyColor := bodyColor;
+  snake.style.headChar := headChar;
+  snake.style.bodyChar := bodyChar;
+end;
+
 procedure InitSnake(var snake: TSnake; fieldSize: TSize);
 begin
 	SetLength(snake.parts, fieldSize.width * fieldSize.height);
-
-	snake.parts[1].x := 2;
-	snake.parts[1].y := 2;
 	
 	snake.length := 2;
-	snake.color := LightGreen;
+	
+	snake.parts[1].x := (fieldSize.width - snake.length) div 2;
+	snake.parts[1].y := fieldSize.height div 2;
 	
 	snake.direction.x := 1;
 	snake.direction.y := 0;	
+	
+	case snake.style.id of
+    1: SetSnakeStyleSettings(snake, Red, Green, ' ', ' ');
+    2: SetSnakeStyleSettings(snake, Green, Red, '0', '1');
+    3: SetSnakeStyleSettings(snake, Yellow, Blue, ' ', ' ');
+  end;
 end;
 
 procedure GetKey(var code: integer);
@@ -295,8 +340,8 @@ begin
   delay(4000);
 end;
 
-procedure HandleCollision(var snake: TSnake; var gameOver: boolean; 
-var food: TFood; fieldSize: TSize);
+procedure HandleCollision(var snake: TSnake; fieldSize: TSize; 
+var gameOver: boolean; var food: TFood);
 var
   snakeHead: TSnakePart;
   i: integer;
@@ -315,31 +360,18 @@ begin
     
   // Border
   if snakeHead.x = fieldSize.width then
-  begin
-    snake.parts[1].x := 2;
-    DrawBorder(fieldSize);
-  end
+    snake.parts[1].x := 2
   else if snakeHead.x = 1 then
-  begin
-    snake.parts[1].x := fieldSize.width - 1;  
-    DrawBorder(fieldSize);  
-  end
+    snake.parts[1].x := fieldSize.width - 1 
   else if snakeHead.y = fieldSize.height then
-  begin
-    snake.parts[1].y := 2;
-    DrawBorder(fieldSize);
-  end
+    snake.parts[1].y := 2
   else if snakeHead.y = 1 then
-  begin
     snake.parts[1].y := fieldSize.height - 1;
-    DrawBorder(fieldSize);
-  end;
   // Food
   if (snakeHead.x = food.pos.x) and (snakeHead.y = food.pos.y) then
   begin
       
     snake.length := snake.length + food.satiation;
-    DrawBorder(fieldSize);
     
     if snake.length >= ((fieldSize.width * fieldSize.height) div 2) then
     begin
@@ -353,6 +385,8 @@ end;
 
 procedure ShowGuide();
 begin
+  clrscr;
+
   TextColor(stdPrintColor);
   write('If you want to ');
   TextColor(LightRed);
@@ -381,37 +415,13 @@ begin
   TextColor(Yellow);
   TextBackground(Black);
   writeln(' - Gives you 3 point');
+  
   writeln;
-end;
-
-procedure ShowWelcomeScreen(var fieldSize: TSize; var snake: TSnake);
-var
-  c: char;
-begin
-  
-  repeat
-    TextColor(stdPrintColor);
-    
-    write('Do you want to start with standard field size (',
-    fieldSize.width,'x',fieldSize.height,')? (y/n): ');
-    
-    TextColor(stdInputColor);
-    readln(c);
-  until (c = 'n') or (c = 'y');
-  
-  if c = 'n' then
-    SetSettings(fieldSize, snake); 
-  
-  writeln();
-  clrscr;
-  GotoXY(1,1);
-  
-  ShowGuide();
   
   TextColor(LightMagenta);
-  write('Enjoy it!');
+  writeln('Enjoy it!');
   writeln;
-  writeln;
+  
   TextColor(stdPrintColor);
   delay(1500);
   write('Press ');
@@ -420,34 +430,74 @@ begin
   TextColor(stdPrintColor);
   writeln(' to start...');
   readln;
+end;
+
+procedure SetSettings(var snake: TSnake; var field: TField);
+var
+  c: char;
+  styleId: integer;
+begin
+  repeat
+    TextColor(stdPrintColor);
+    
+    write('Do you want to start with standard settings? (y/n): ');
+    
+    TextColor(stdInputColor);
+    readln(c);
+  until (c = 'n') or (c = 'y');
   
-  clrscr;
+  if c = 'n' then
+  begin
+    
+    TextColor(stdPrintColor);
+    write('Field width[',minFieldWidth,' - ', ScreenWidth - 5,']: ');
+    ReadInteger(minFieldWidth, ScreenWidth - 5, field.size.width, 
+    'You entered an incorrect field width');
+    
+    TextColor(stdPrintColor);
+    write('Field height[',minFieldHeight,' - ', ScreenHeight - 2,']: ');
+    ReadInteger(minFieldHeight, ScreenHeight - 2, field.size.height, 
+    'You entered an incorrect field height');
+
+     
+    TextColor(stdPrintColor);
+    write('Select style[',1,' - ', maxStyleId,']: ');
+    ReadInteger(1, maxStyleId, styleId, 
+    'You entered style that doesn''t exist' );
+     
+  end
+  else
+  begin
+    styleId := 1;
+    field.size.width := 20;
+    field.size.height := 14;
+  end;
+  
+  snake.style.id := styleId;
+  field.style.id := styleId;
 end;
 
 var
 	snake: TSnake;
 	food: TFood;	
-	fieldSize: TSize;
+	field: TField;
 	gameOver: boolean;
 	
 begin
-  fieldSize.width := 30;
-  fieldSize.height := 18;
-  
   randomize;
   gameOver := false;
   clrscr;
   
-  InitSnake(snake, fieldSize);
-
-  ShowWelcomeScreen(fieldSize, snake);
+  SetSettings(snake, field);
+  ShowGuide();
   
   clrscr;
   
-  UpdateFood(snake, food, fieldSize);
+  InitSnake(snake, field.size);
+  InitField(field);
   
-  DrawBorder(fieldSize);
-  
+  UpdateFood(snake, food, field.size);
+
 	while not gameOver do
 	begin
     delay(delayDuration);
@@ -456,9 +506,11 @@ begin
       HandleKey(snake, gameOver);
     
     UpdateSnake(snake);
-    HandleCollision(snake, gameOver, food, fieldSize);
-    
+    HandleCollision(snake, field.size, gameOver, food);
+      DrawField(field);
+  
 	end;
+	
 	
 	write(#27'[0m');
 	clrscr;
